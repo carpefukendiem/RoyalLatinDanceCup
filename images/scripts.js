06b66d0f5b9de5947da5877bf91a59dd72a4932f
carpefukendiem <rubenstips@gmail.com>
2024-04-26 09:05:52 -0700

    made more changes to site, almost complete

--- images/scripts.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    const header = document.getElementById('header');
-    if (window.scrollY > 0) {
-      header.classList.add('header-scrolled');
-    } else {
-      header.classList.remove('header-scrolled');
-    }
-  });
